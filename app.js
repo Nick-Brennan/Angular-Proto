120d83259e93f723bd20125f6e0861791bedb8d8
Nick Brennan <nick_p_b@yahoo.com>
2015-10-18 15:22:09 -0700

    working on route suggestion functionality

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ app.controller('indexCtrl', ['$scope', '$sce', function($scope, $sce){
     $scope.plan.miles = 0;
     $scope.plan.days = 0;
     $scope.plan.total = 0;
+    $scope.plan.runs = runs;
+    console.log(runs);
 //    $scope.plan.monUrl = $sce.trustAsResourceUrl('https://www.runtastic.com/en/routes#/search?nw_latitude=37.96763381521261&nw_longitude=-122.57988285583491&se_latitude=37.930408444176386&se_longitude=-122.51482319396968&zoom_level=14&clat=37.94902348734078&clng=-122.54735302490229&distance_min=0&distance_max=482803&elevation_gain_min=0&elevation_gain_max=762&sport_type_id=1&page=1');
 //    $scope.trustSrc = function(src) {
 //        return $sce.trustAsResourceUrl(src);
 //    }
    
     $scope.calcTotal = function(){
+        
+        var findRun = function(dist){
+            var res = [];
+            $scope.plan.runs.forEach(function(run){
+                if(run.distance >= (dist - 0.5) && run.distance <= (dist + 0.5)){
+                    res.push(run);
+                }
+            });
+            return res;
+        };
+        
+        
         $scope.plan.days = (($scope.plan.monday ? 1 : 0)
                                                       + ($scope.plan.tuesday ? 1 : 0)
                                                       + ($scope.plan.wednesday ? 1 : 0)
@@ -32,6 +46,14 @@ app.controller('indexCtrl', ['$scope', '$sce', function($scope, $sce){
             $scope.plan.thursData = $scope.plan.thursday ? weekMiles : 0;
             $scope.plan.friData = $scope.plan.friday ? weekMiles : 0;
             
+            $scope.plan.satUrl = findRun($scope.plan.satData)[0].map;
+            $scope.plan.sunUrl = findRun($scope.plan.sunData)[0].map;
+            $scope.plan.monUrl = $scope.plan.monday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.tuesUrl = $scope.plan.tuesday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.wedsUrl = $scope.plan.wednesday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.thursUrl = $scope.plan.thursday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.friUrl = $scope.plan.friday ? findRun(weekMiles)[0].map : null;
+            
         }else if(($scope.plan.saturday || $scope.plan.sunday) && ($scope.plan.days > 3)){
             weekendMiles = $scope.plan.miles * .30;
             weekMiles = ($scope.plan.miles * .7) / ($scope.plan.days - 1);
@@ -42,6 +64,14 @@ app.controller('indexCtrl', ['$scope', '$sce', function($scope, $sce){
             $scope.plan.friData = $scope.plan.friday ? weekMiles : 0;
             $scope.plan.satData = $scope.plan.saturday ? weekendMiles : 0;
             $scope.plan.sunData = $scope.plan.sunday ? weekendMiles : 0;
+            
+            $scope.plan.satUrl = $scope.plan.saturday ? findRun(weekendMiles)[0].map : null;
+            $scope.plan.sunUrl = $scope.plan.sunday ? findRun(weekendMiles)[0].map : null;
+            $scope.plan.monUrl = $scope.plan.monday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.tuesUrl = $scope.plan.tuesday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.wedsUrl = $scope.plan.wednesday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.thursUrl = $scope.plan.thursday ? findRun(weekMiles)[0].map : null;
+            $scope.plan.friUrl = $scope.plan.friday ? findRun(weekMiles)[0].map : null;
         }else{
             $scope.plan.total =  $scope.plan.miles / $scope.plan.days;
             $scope.plan.monData = $scope.plan.monday ? $scope.plan.total : 0;
@@ -51,7 +81,17 @@ app.controller('indexCtrl', ['$scope', '$sce', function($scope, $sce){
             $scope.plan.friData = $scope.plan.friday ? $scope.plan.total : 0;
             $scope.plan.satData = $scope.plan.saturday ? $scope.plan.total : 0;
             $scope.plan.sunData = $scope.plan.sunday ? $scope.plan.total : 0;
+            
+            $scope.plan.satUrl = $scope.plan.saturday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.sunUrl = $scope.plan.sunday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.monUrl = $scope.plan.monday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.tuesUrl = $scope.plan.tuesday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.wedsUrl = $scope.plan.wednesday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.thursUrl = $scope.plan.thursday ? findRun($scope.plan.total)[0].map : null;
+            $scope.plan.friUrl = $scope.plan.friday ? findRun($scope.plan.total)[0].map : null;
         }
+        
+        
     };
 }]);
 
@@ -60,3 +100,5 @@ app.filter("distance", function(){
         return input + " miles";
     };
 });
+
+//app.service("findRun", )
--- data.js
+++ data.js
@@ -1,90 +1,102 @@
 var runs = [{
         distance: 13.9,
-        map: 'assets/14_TennesseeValley.png',
+        map: 'assets/14_tennesseevalley.png',
         url: 'https://www.strava.com/activities/414877252',
         city: 'Mill Valley'
     },
     {
         distance: 21.9,
-        map: 'assets/22_MuirWoods.png',
+        map: 'assets/22_muirwoods.png',
         url: 'https://www.strava.com/activities/414418429',
         city: 'Mill Valley'
     },
     {
         distance: 8.1,
-        map: 'assets/8_Homestead.png',
+        map: 'assets/8_homestead.png',
         url: 'https://www.strava.com/activities/411262876',
         city: 'Mill Valley'
     },
     {
         distance: 2.0,
-        map: 'assets/2_Homestead.png',
+        map: 'assets/2_homestead.png',
         url: 'https://www.strava.com/activities/405068621',
         city: "Mill Valley"
     },
     {
         distance: 6.2,
-        map: '6_MillValley.png',
+        map: 'assets/6_millvalley.png',
         url: 'https://www.strava.com/activities/402488920',
         city: 'Mill Valley'
     },
     {
         distance: 5.0,
-        map: 'assets/5_Homestead.png',
+        map: 'assets/5_homestead.png',
         url: 'https://www.strava.com/activities/402476255',
         city: 'Mill Valley'
     },
     {
         distance: 5.2,
-        map: 'assets/5_MillValley.png',
+        map: 'assets/5_millvalley.png',
         url: 'https://www.strava.com/activities/396783171',
         city: 'Mill Valley'
     },
     {
         distance: 13.1,
-        map: 'assets/13_MtTam.png',
+        map: 'assets/13_mttam.png',
         url: 'https://www.strava.com/activities/390774579',
         city: 'Mill Valley'
     },
     {
         distance: 4.4,
-        map: 'assets/4_MillValley.png',
+        map: 'assets/4_millvalley.png',
         url: 'https://www.strava.com/activities/384433226',
         city: 'Mill Valley'
     },
     {
         distance: 3.4,
-        map: 'assets/3_MillValley.png',
+        map: 'assets/3_millvalley.png',
         url: 'https://www.strava.com/activities/376811436',
         city: 'Mill Valley'
     },
     {
         distance: 15.0,
-        map: 'assets/15_TennesseeValley.png',
+        map: 'assets/15_tennesseevalley.png',
         url: 'https://www.strava.com/activities/361476760',
         city: 'Mill Valley'
     },
     {
-        distance: 9.5,
-        map: 'assets/9_MillValley.png',
+        distance: 9.4,
+        map: 'assets/9_millvalley.png',
         url: 'https://www.strava.com/activities/357626372',
         city: 'Mill Valley'
     },
     {
         distance: 9.9,
-        map: 'assets/10_MtTam.png',
+        map: 'assets/10_mttam.png',
         url: 'https://www.strava.com/activities/342043583',
         city: 'Mill Valley'
     },
     {
         distance: 11.5,
-        map: 'assets/11_Sausalito.png',
+        map: 'assets/11_sausalito.png',
         url: 'https://www.strava.com/activities/415101154',
         city: 'Mill Valley'
     },
     {
         distance: 12.4,
-        map: 'assets/12_TennesseeValley.png',
+        map: 'assets/12_tennesseevalley.png',
         url: 'https://www.strava.com/activities/414861111',
+        city: 'Mill Valley'
+    },
+    {
+        distance: 10.7,
+        map: 'assets/10_tennesseevalley.png',
+        url: 'https://www.strava.com/activities/415736792',
+        city: 'Mill Valley'
+    },
+    {
+        distance: 7.3,
+        map: 'assets/7_millvalley.png',
+        url: 'https://www.strava.com/activities/364524117',
         city: 'Mill Valley'
     }];
