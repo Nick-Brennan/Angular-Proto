89e2ff44bfa7bb3dcc2b033d370e00cc7f4d0a09
Nick Brennan <nick_p_b@yahoo.com>
2015-11-04 17:38:04 -0800

    trying path change for map images

--- data.js
+++ data.js
@@ -1,150 +1,150 @@
 var runs = [{
         distance: 13.9,
-        map: 'assets/14_tennesseevalley.png',
+        map: '../assets/14_tennesseevalley.png',
         url: 'https://www.strava.com/activities/414877252',
         city: 'Mill Valley'
     },
     {
         distance: 21.9,
-        map: 'assets/22_muirwoods.png',
+        map: '../assets/22_muirwoods.png',
         url: 'https://www.strava.com/activities/414418429',
         city: 'Mill Valley'
     },
     {
         distance: 8.1,
-        map: 'assets/8_homestead.png',
+        map: '../assets/8_homestead.png',
         url: 'https://www.strava.com/activities/411262876',
         city: 'Mill Valley'
     },
     {
         distance: 2.0,
-        map: 'assets/2_homestead.png',
+        map: '../assets/2_homestead.png',
         url: 'https://www.strava.com/activities/405068621',
         city: "Mill Valley"
     },
     {
         distance: 6.2,
-        map: 'assets/6_millvalley.png',
+        map: '../assets/6_millvalley.png',
         url: 'https://www.strava.com/activities/402488920',
         city: 'Mill Valley'
     },
     {
         distance: 5.0,
-        map: 'assets/5_homestead.png',
+        map: '../assets/5_homestead.png',
         url: 'https://www.strava.com/activities/402476255',
         city: 'Mill Valley'
     },
     {
         distance: 5.2,
-        map: 'assets/5_millvalley.png',
+        map: '../assets/5_millvalley.png',
         url: 'https://www.strava.com/activities/396783171',
         city: 'Mill Valley'
     },
     {
         distance: 13.1,
-        map: 'assets/13_mttam.png',
+        map: '../assets/13_mttam.png',
         url: 'https://www.strava.com/activities/390774579',
         city: 'Mill Valley'
     },
     {
         distance: 4.4,
-        map: 'assets/4_millvalley.png',
+        map: '../assets/4_millvalley.png',
         url: 'https://www.strava.com/activities/384433226',
         city: 'Mill Valley'
     },
     {
         distance: 3.4,
-        map: 'assets/3_millvalley.png',
+        map: '../assets/3_millvalley.png',
         url: 'https://www.strava.com/activities/376811436',
         city: 'Mill Valley'
     },
     {
         distance: 15.0,
-        map: 'assets/15_tennesseevalley.png',
+        map: '../assets/15_tennesseevalley.png',
         url: 'https://www.strava.com/activities/361476760',
         city: 'Mill Valley'
     },
     {
         distance: 9.4,
-        map: 'assets/9_millvalley.png',
+        map: '../assets/9_millvalley.png',
         url: 'https://www.strava.com/activities/357626372',
         city: 'Mill Valley'
     },
     {
         distance: 9.9,
-        map: 'assets/10_mttam.png',
+        map: '../assets/10_mttam.png',
         url: 'https://www.strava.com/activities/342043583',
         city: 'Mill Valley'
     },
     {
         distance: 11.5,
-        map: 'assets/11_sausalito.png',
+        map: '../assets/11_sausalito.png',
         url: 'https://www.strava.com/activities/415101154',
         city: 'Mill Valley'
     },
     {
         distance: 12.4,
-        map: 'assets/12_tennesseevalley.png',
+        map: '../assets/12_tennesseevalley.png',
         url: 'https://www.strava.com/activities/414861111',
         city: 'Mill Valley'
     },
     {
         distance: 10.7,
-        map: 'assets/10_tennesseevalley.png',
+        map: '../assets/10_tennesseevalley.png',
         url: 'https://www.strava.com/activities/415736792',
         city: 'Mill Valley'
     },
     {
         distance: 7.3,
-        map: 'assets/7_millvalley.png',
+        map: '../assets/7_millvalley.png',
         url: 'https://www.strava.com/activities/364524117',
         city: 'Mill Valley'
     },
     {
         distance: 1.0,
-        map: 'assets/1_homestead.png',
+        map: '../assets/1_homestead.png',
         url: 'https://www.strava.com/activities/353339636',
         city: "Mill Valley"
     },
     {
         distance: 8.4,
-        map: 'assets/8_5_mttam.png',
+        map: '../assets/8_5_mttam.png',
         url: 'https://www.strava.com/activities/356954755',
         city: 'Mill Valley'
     },
     {
         distance: 3.0,
-        map: 'assets/3_homestead.png',
+        map: '../assets/3_homestead.png',
         url: 'https://www.strava.com/activities/302063565',
         city: 'Mill Valley'
     },
     {
         distance: 6.8,
-        map: 'assets/7_dipsea.png',
+        map: '../assets/7_dipsea.png',
         url: 'https://www.strava.com/activities/325657636',
         city: 'Mill Valley'
     },
     {
         distance: 14.5,
-        map: 'assets/14_5_sausalito.png',
+        map: '../assets/14_5_sausalito.png',
         url: 'https://www.strava.com/activities/353339775',
         city: 'Mill Valley'
     },
     { 
         distance: 17.0,
-        map: 'assets/17_stinson.png',
+        map: '../assets/17_stinson.png',
         url: 'https://www.strava.com/activities/415707165',
         city: 'Mill Valley'
     },
     {
         distance: 16.0,
-        map: 'assets/16_mttam.png',
+        map: '../assets/16_mttam.png',
         url: 'https://www.strava.com/activities/415802940',
         city: 'Mill Valley'
     },
     {
         distance: 18.0,
-        map: 'assets/18_millvalley.png',
+        map: '../assets/18_millvalley.png',
         url: 'https://www.strava.com/activities/414881449',
         city: 'Mill Valley'
     }];
